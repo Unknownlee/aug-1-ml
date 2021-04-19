4beb10b9d3e7290639548397a86c3011d5f06fd9
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-04-19 16:58:06 +0000

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -25,4 +25,6 @@ async def help_message_f(client, message):
     # display the /help
 
     await message.reply_text(
-        <b>Hello 👾 !This is Telegram Leech bot 🧲  Click Below to know how to use me 📝Developer 👨🏻‍💻: @Itz_Me_Malayaali</b>
+                # <b>Hello 👾!
+                # This is Telegram Leech bot 🧲  Click Below to know how to use me 📝
+                # Developer 👨🏻‍💻: @Itz_Me_Malayaali</b>
