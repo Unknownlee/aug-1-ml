d581505de71214077eb40f85314a28ac258a53fe
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-04-19 17:13:57 +0000

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -25,33 +25,6 @@ async def help_message_f(client, message):
     # display the /help
 
     await message.reply_text(
-                ##!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
-
-import logging
-
-import pyrogram
-from tobrot import AUTH_CHANNEL, LOGGER
-
-
-async def new_join_f(client, message):
-    chat_type = message.chat.type
-    if chat_type != "private":
-        await message.reply_text(f"Current CHAT ID: <code>{message.chat.id}</code>")
-        # leave chat
-        await client.leave_chat(chat_id=message.chat.id, delete=True)
-    # delete all other messages, except for AUTH_CHANNEL
-    await message.delete(revoke=True)
-
-
-async def help_message_f(client, message):
-    # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
-    # channel_id = str(AUTH_CHANNEL)[4:]
-    # message_id = 99
-    # display the /help
-
-    await message.reply_text(
         """Available Commands
 /rclone: This will change your drive config on fly.(First one will be default)
 
@@ -106,4 +79,3 @@ How to Use?
 send any one of the available command, as a reply to a valid link/magnet/torrent. 👊""",
         disable_web_page_preview=True,
     )
-
